96451c2c50a2dabb2900a4ae4c81fd5a9d7e6afe
Stautob <stautob@stautob.ch>
2016-05-07 12:45:44 +0200

    Added first vector_helper test

---
+++ test/vector_helpers_test.cpp
@@ -0,0 +1,32 @@
+#include "util/vector_helpers.h"
+#include "common/common_types.h"
+
+#include <cute/cute.h>
+#include <cute/cute_runner.h>
+#include <cute/cute_suite.h>
+#include <cute/xml_listener.h>
+#include <cute/ide_listener.h>
+
+
+using namespace dabip;
+void test_split_vector()
+  {
+  byte_vector_t input {0x12, 0x34, 0x56, 0x78, 0x90};
+  auto splitted = split_vector(input, 3);
+  byte_vector_t should_first = byte_vector_t{0x12, 0x34, 0x56};
+  byte_vector_t should_second = byte_vector_t{0x78, 0x90};
+  ASSERT(splitted.first  == should_first);
+  ASSERT(splitted.second == should_second);
+  }
+
+int main(int argc, char * * argv)
+  {
+  auto testSuite = cute::suite{};
+
+  testSuite += cute::test{test_split_vector, "Test split_vector"};
+
+  auto xmlFile = cute::xml_file_opener{argc, argv};
+  auto listener = cute::xml_listener<cute::ide_listener<>>{xmlFile.out};
+
+  return !cute::makeRunner(listener, argc, argv)(testSuite, "Vector_helpers test suite");
+  }
