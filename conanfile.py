619e8bb6697b5030ee63d2b430d9a618f8fe392a
Felix Morgner <fmorgner@hsr.ch>
2017-08-03 18:45:14 +0200

    cmake: stop hiding symbols

--- conanfile.py
+++ conanfile.py
@@ -15,7 +15,7 @@ class LibDABIPConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        lib = '-DBUILD_SHARED_LIBS=%s' % 'On' if self.options.shared else 'Off'
+        lib = '-DBUILD_SHARED_LIBS=%s' % ('On' if self.options.shared else 'Off')
         args = [lib, '-DCMAKE_INSTALL_PREFIX="%s"' % self.package_folder]
         self.run('cmake %s %s %s'
                  % (self.source_folder,
